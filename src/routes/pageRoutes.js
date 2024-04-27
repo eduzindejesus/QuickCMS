9f11fc3bf98af3355714676b50fe244e61a0980f
Guilherme Sebastião <guivbsebas@gmail.com>
2024-04-27 15:53:26 -0300

    Adiciona novo estilo para `login.ejs` e muda o path de `pagesRoutes.js`

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pageController = require('../controllers/pageController');
+const pageController = require("../controllers/pageController");
 
-//rotas
-router.get('/login', pageController.renderLoginPage); // Rota para página de login
-router.post('/login', pageController.login); // Rota para processo de login (POST)
-router.get('/logout', pageController.logout); // Rota para processo de logout
+// Rotas
+router.get("/login", pageController.renderLoginPage); // Rota para página de login
+router.post("/login", pageController.login); // Rota para processo de login (POST)
+router.get("/logout", pageController.logout); // Rota para processo de logout
 
-router.get('/', pageController.listPages); // Listar todas as páginas
-router.post('/', pageController.createPage); // Criar uma nova página
-router.get('/:pageUrl', pageController.getPageByUrl); // Obter uma página por URL
-router.put('/:pageUrl', pageController.updatePage); // Atualizar o conteúdo de uma página
-router.delete('/:pageUrl', pageController.deletePage); // Excluir uma página
+router.get("/", pageController.listPages); // Listar todas as páginas
+router.post("/", pageController.createPage); // Criar uma nova página
+router.get("/:pageUrl", pageController.getPageByUrl); // Obter uma página por URL
+router.put("/:pageUrl", pageController.updatePage); // Atualizar o conteúdo de uma página
+router.delete("/:pageUrl", pageController.deletePage); // Excluir uma página
 
 module.exports = router;
